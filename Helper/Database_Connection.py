6fa439dbd0b3530a8eae1a347fd8f57932ab4b98
Bala Yeruva <madhur2k9@gmail.com>
2017-10-28 22:13:22 -0400

    Created Flask routes

--- Helper/Database_Connection.py
+++ Helper/Database_Connection.py
@@ -2,8 +2,10 @@ from mysql.connector import MySQLConnection, Error
 from Helper.Database_Config import read_db_config
 import datetime
 import Helper.EM_Utility as emu
+import Models.User
 
 apath = None
+user = None
 
 def set_config_file_path(path):
     global apath
@@ -68,7 +70,7 @@ def email_exists(email):
 # For Login
 def login_user(email, password):
     if email_exists(email):
-        query = 'SELECT password from user_accounts WHERE email = %s'
+        query = 'SELECT * from user_accounts WHERE email = %s'
         args = (email,)
         db = connect()
         cursor = db.cursor()
@@ -79,12 +81,87 @@ def login_user(email, password):
         cursor.close()
         db.close()
 
-        if row[0] == emu.hash_password(password):
+        if row[2] == emu.hash_password(password):
+            global user
+            user = Models.User.User(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
             return True
         return False
 
     else:
         return False
+
+def get_user_id(email):
+    if user is not None:
+        return user.get_id()
+    else:
+        return -999
+
+def record_transaction(transac_type, category, title, description, amount, user_id):
+    query = 'INSERT INTO user_transactions (transaction_type, category, title, description, amount, date_added, user_id)' \
+            'VALUES (%s,%s,%s,%s,%s,%s,%s)'
+    current_date = datetime.datetime.now()
+    args = (transac_type, category, title, description, amount, current_date, user_id)
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute(query,args)
+    # TO DO Refactor the following 3 lines
+    db.commit()
+    cursor.close()
+    db.close()
+
+def get_all_transactions(user_id):
+    query = 'SELECT * from user_transactions WHERE user_id = %s'
+    args = (user_id,)
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute(query, args)
+    rows = None
+    rows = cursor.fetchall()
+    db.commit()
+    cursor.close()
+    db.close()
+    transactions = {}
+    for row in rows:
+        transactions[row[0]] = list(row[1:])
+    return transactions
+
+def get_all_expenses(user_id):
+    query = 'SELECT * from user_transactions WHERE user_id = %s AND transaction_type = %s'
+    args = (user_id, 0)
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute(query, args)
+    rows = None
+    rows = cursor.fetchall()
+    db.commit()
+    cursor.close()
+    db.close()
+    transactions = {}
+    for row in rows:
+        transactions[row[0]] = list(row[1:])
+    return transactions
+
+def get_all_incomes(user_id):
+    query = 'SELECT * from user_transactions WHERE user_id = %s AND transaction_type = %s'
+    args = (user_id, 1)
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute(query, args)
+    rows = None
+    rows = cursor.fetchall()
+    db.commit()
+    cursor.close()
+    db.close()
+    transactions = {}
+    for row in rows:
+        transactions[row[0]] = list(row[1:])
+    return transactions
+
+def get_user():
+    if user is not None:
+        return user
+    else:
+        return -999
 
 """if __name__ == '__main__':
     insert_new_user('asdf','asdf','asdfsa')"""
--- expense_manager.py
+++ expense_manager.py
@@ -1,12 +1,59 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
+from Helper.Database_Connection import insert_new_user, set_config_file_path, login_user, record_transaction, \
+    get_all_transactions, get_all_expenses, get_all_incomes, get_user_id, get_user
+from Models.User import User
+
+set_config_file_path('config.ini')
 
 app = Flask(__name__)
 
+user = None
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/register', methods = ['POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        message = 'Account successfully created!' if insert_new_user(name,email,password) else 'Email already exists'
+        status = {'message': message}
+        return jsonify(status)
+
+@app.route('/login', methods = ['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        result = login_user(email,password)
+        if result:
+            global user
+            user = get_user()
+            session['id'] = user.get_id()
+            message = 'Login successful! :)'
+        else:
+            message = 'Invalid ID password combo :('
+        status = {'message': message}
+        return jsonify(status)
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('id')
+    global user
+    user = None
+
+@app.route('/getBalance', methods=['GET'])
+def get_balance():
+    global user
+    if user is not None:
+        return jsonify({'new_balance': user.get_current_balance()})
+    return jsonify({"message":"Please login!"})
+
+
 
 if __name__ == '__main__':
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run()
